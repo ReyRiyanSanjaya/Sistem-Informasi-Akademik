b8ea6c07f58990c8d9e93438964a39165d756c97
Rey Riyan Sanjaya <reyriyansanjaya>
2019-12-06 10:27:57 -0800

    pembaruan

---
+++ src/loader.php
@@ -0,0 +1,14 @@
+<div class="wrapper">
+  
+  <div id="loader-wrapper">
+  
+    <div id="loader">
+   <p>LOADING</p>
+  <div class="circ-one"></div><div class="circ-two"></div>
+</div>
+
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
+
+ </div>
+
